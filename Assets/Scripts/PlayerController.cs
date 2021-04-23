294c91d52f305aa0bb7d9df89040448406193bf0
ithras <labarca123456@gmail.com>
2021-04-23 18:16:09 -0400

    basic scripting

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public Transform currentCheckpointTransform;
+
+    private int currentCheckpoint = 0;
+
+    private bool beenHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void resetPlayer() 
+    {
+        gameObject.transform.position = currentCheckpointTransform.position;
+        gameObject.transform.rotation.SetLookRotation(Vector3.zero);
+    }
+}
