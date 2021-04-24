4573425175ea1ffb1145bf24179ec0bf99e91e06
ithras <labarca123456@gmail.com>
2021-04-23 20:49:58 -0400

    Labarca - Script de movimiento + Prefabs con colliders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,26 +6,34 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public Transform currentCheckpointTransform;
-
-    private int currentCheckpoint = 0;
-
-    private bool beenHit;
+    private Vector2 movement;
+    private Rigidbody2D playerRB;
+    private Rigidbody2D boxRB;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        movement = Vector2.zero;
+        movement.y = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+
         
+        playerRB.velocity = movement * speed;
     }
 
-    void resetPlayer() 
+    void OnCollisionEnter2D(Collision2D col)
     {
-        gameObject.transform.position = currentCheckpointTransform.position;
-        gameObject.transform.rotation.SetLookRotation(Vector3.zero);
+        if (!col.gameObject.CompareTag("Floor") && !col.gameObject.CompareTag("Box"))
+            gameObject.GetComponent<ResetPosition>().resetPosition();
+
+        if(col.gameObject.CompareTag("Box")){
+            col.gameObject.GetComponent<Rigidbody2D>().AddForce(playerRB.velocity);
+        }
     }
 }
---
+++ Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ResetPosition : MonoBehaviour
+{
+    public Transform originalPosition;
+    
+    public void resetPosition() 
+    {
+        gameObject.transform.position = originalPosition.position;
+    }
+}
