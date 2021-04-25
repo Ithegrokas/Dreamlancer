7491755870126daaeea032140fd5eb09ecb299df
ithras <labarca123456@gmail.com>
2021-04-24 22:24:54 -0400

    More prefab fix. Player sprite flip added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 5.0f;
     private Vector2 movement;
     private Rigidbody2D playerRB;
+    private SpriteRenderer playerSPR;
+
+    private bool isFlipped;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        playerSPR = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -23,5 +27,13 @@ public class PlayerController : MonoBehaviour
 
 
         playerRB.velocity = movement * speed;
+
+        if (movement.x < 0 && !playerSPR.flipX)
+            playerSPR.flipX = true;
+        
+        else if (movement.x >=0 && playerSPR.flipX) 
+            playerSPR.flipX = false;
+        
+            
     }
 }
