2b9e70946e257bfbb28da42a7fd314007f82b7e3
ithras <labarca123456@gmail.com>
2021-04-26 14:32:10 -0400

    ASD

--- Assets/Scripts/OldLadyTrigger.cs
+++ Assets/Scripts/OldLadyTrigger.cs
@@ -6,4 +6,12 @@ public class OldLadyTrigger : MonoBehaviour
 {
     [SerializeField] private Animator oldLady;
     [SerializeField] private Animator boxKey;
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if(col.CompareTag("Key")){
+            oldLady.gameObject.SetActive(true);
+            boxKey.SetBool("asd",true);
+        }
+
+    }
 }
