7455acd77324b990c2cbda64cc9b6175e1f526bc
ithras <labarca123456@gmail.com>
2021-04-26 06:40:19 -0400

    Labarca - George

--- Assets/Scripts/Pedestal.cs
+++ Assets/Scripts/Pedestal.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Pedestal : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer crystalRenderer = null;
+    [SerializeField] private Light2D crystalLight = null;
+    
 
     public void changeCrystalColor(Color color)
     {
         crystalRenderer.color = color;
+        crystalLight.color = color;
     }
 }
