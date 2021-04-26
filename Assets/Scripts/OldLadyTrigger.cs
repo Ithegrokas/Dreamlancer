6e664198e6f6d3ffd0722840893a44533dfc1170
ithras <labarca123456@gmail.com>
2021-04-26 14:28:12 -0400

    Labarca - asdojaod

---
+++ Assets/Scripts/OldLadyTrigger.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldLadyTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator oldLady;
+    [SerializeField] private Animator boxKey;
+}
